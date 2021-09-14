02607d1405f41fe8349c5c4d1ad503fe4ebb6d5c
Kuda-ux <kudakwasherapozo@yahoo.com>
2021-09-14 03:02:27 +0000

    File updates

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+(sentence):
+    len_sen = len(sentence)
+
+    if (len_sen == 0):
+        new_tuple = (len_sen, None)
+    else:
+        new_tuple = (len_sen, sentence[0])
+
+    return (new_tuple)
+
